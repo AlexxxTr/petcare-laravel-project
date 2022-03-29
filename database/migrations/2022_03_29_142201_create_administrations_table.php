7473f63ff2d2ca6a4305c33ce30c1064838f9376
Alexander Truyaert <alexander.truyaert@outlook.com>
2022-03-29 17:19:06 +0200

    AT: Added some small mulitlingual to the database

--- database/migrations/2022_03_29_142201_create_administrations_table.php
+++ database/migrations/2022_03_29_142201_create_administrations_table.php
@@ -14,10 +14,20 @@ return new class extends Migration
     public function up()
     {
         Schema::create('administrations', function (Blueprint $table) {
+            $table->id();
             $table->integer('pet_id');
             $table->date('date');
             $table->string('meal');
+            $table->string('notes'); // Leaving it here for NodeJs server
             $table->integer('medicine_id');
+        });
+
+        Schema::create('administrations_language', function (Blueprint $table) {
+            $table->id();
+            $table->integer('adminstration_id');
+            $table->date('date');
+            $table->string('meal');
+            $table->string('notes');
         });
     }
 
