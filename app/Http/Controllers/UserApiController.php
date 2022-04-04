50f097a870785ed537718f8357f3ee294c677ea2
Alexander Truyaert <alexander.truyaert@outlook.com>
2022-04-04 12:25:20 +0200

    AT: created the first route (doesn't work yet, commit to continue with other stuff)

---
+++ app/Http/Controllers/UserApiController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Modules\Users\Services\UserService;
+use Illuminate\Http\Request;
+
+class UserApiController extends Controller
+{
+    private $service;
+
+    public function __construct(UserService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function getUserById($id)
+    {
+        $user = $this->service->getUserById($id);
+
+        if (is_null($user)) return response(['error' => 'user not found'], 404);
+
+        return $user;
+    }
+}
+     
---
+++ app/Modules/Users/Services/UserSevice.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Modules\Users\Services;
+
+use App\Modules\Core\Services\Service;
+use App\Modules\Users\Models\User;
+use Illuminate\Validation\Rules\Password;
+
+class UserService extends Service
+{
+    protected $rules = [
+        "name" => ["string", "required"],
+        "email" => ["string", "required", "email"],
+        "password" => ["required"/*,  Password::min(8)->mixedCase()->numbers()*/]
+    ];
+
+    public function __construct(User $model)
+    {
+        parent::__construct($model);   
+    }
+
+    public function getUserById($id) {
+        return $this->model->first($id)->get();
+    }
+}
