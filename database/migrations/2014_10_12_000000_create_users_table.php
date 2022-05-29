70cebede07bf5f294983f146af7ef4be3474d3e9
Alexander Truyaert <alexander.truyaert@outlook.com>
2022-05-29 10:44:29 +0200

    AT: removed unique from migrations is checked in service anyway

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->integer('id')->autoIncrement();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('password');
         });
     }
